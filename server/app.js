178cb8e78798b62291ee92c9f068a891ab578e60
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-01-08 19:53:40 +0500

    node modules

--- server/app.js
+++ server/app.js
@@ -1,2 +1,24 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const port = 5000;
+
+app.use(express.json());
+
+// app.use("/auth", );
+
+const runServer = async () => {
+  try {
+    await mongoose.connect();
+
+
+    
+    app.listen(port, () => {
+      console.log(`App is listening ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runServer();
