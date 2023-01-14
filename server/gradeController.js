8898bb218c3d628082ed13b0c25190fe0e30541d
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-01-14 16:27:30 +0500

    feat

--- server/gradeController.js
+++ server/gradeController.js
@@ -2,7 +2,8 @@ const gradingHomework = async (req, res) => {
   const { whose, link } = req.body;
   const isExist = await User.findOne({ whose });
 
-  if (isExist) {
-    return res.json({ message: "Не корректно указанно чье дз" });
+  if (!isExist) {
+    return res.json({ message: "Не можем узнать чье дз" });
   }
+  
 };
--- server/models/PostHomework.js
+++ server/models/PostHomework.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const PostHomework = new Schema({
-  link: { type: String, required: true, ref: "Post" },
+  link: { type: String, required: true, ref: "Post", unique: true },
   score: { type: String, default: "Fail", required: false },
   whose: { type: String, required: true, unique: true },
 });
