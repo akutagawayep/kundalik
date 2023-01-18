6cdc88ef646bb402a2f141356619dad4d20b7eb9
akutagawayep <dilnoza.akhmedova.icloud@gmail.com>
2023-01-18 13:53:21 +0500

    refactoring

--- client/src/pages/home/homeComponents/StudentsBlock/index.jsx
+++ client/src/pages/home/homeComponents/StudentsBlock/index.jsx
@@ -5,49 +5,61 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slide from "./Slide/index";
 import sumMen from "../StartingBlock/img/sumMen.png";
-import './slider.scss'
+import "./slider.scss";
 
 const Students = () => {
+  
+  const number = (e) => {
+    if (e > 1400) {
+      return 3;
+    } else if (e > 1200) {
+      return 2;
+    } else return 1;
+  };
+ let  amount = number(window.innerWidth);
+
   var settings = {
     draggable: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: amount,
     slidesToScroll: 1,
-    // prevArrow:"->"
   };
   return (
     <div className={s.root}>
-      <Slider {...settings}>
-        <Slide
-          name="Nasir jamshed"
-          img={sumMen}
-          text="A skill is the learned ability to perform 
+      <h2>Students review</h2>
+      <div className={s.container}>
+        <Slider {...settings}>
+          <Slide
+            name="Nasir jamshed"
+            img={sumMen}
+            text="A skill is the learned ability to perform 
 an action with determined results with 
 Execution often Within given amount "
-        />
-        <Slide
-          name="Nasir jamshed"
-          img={sumMen}
-          text="A skill is the learned ability to perform 
+          />
+          <Slide
+            name="Nasir jamshed"
+            img={sumMen}
+            text="A skill is the learned ability to perform 
 an action with determined results with 
 Execution often Within given amount "
-        />
-        <Slide
-          name="Nasir jamshed"
-          img={sumMen}
-          text="A skill is the learned ability to perform 
+          />
+          <Slide
+            name="Nasir jamshed"
+            img={sumMen}
+            text="A skill is the learned ability to perform 
 an action with determined results with 
 Execution often Within given amount "
-        />
-        <Slide
-          name="Nasir jamshed"
-          img={sumMen}
-          text="A skill is the learned ability to perform 
+          />
+          <Slide
+            name="Nasir jamshed"
+            img={sumMen}
+            text="A skill is the learned ability to perform 
 an action with determined results with 
 Execution often Within given amount "
-        />
-      </Slider>
+          />
+        </Slider>
+      </div>
     </div>
   );
 };
