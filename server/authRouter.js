e4e377d5ebbf4407d5ad57510d85c779c956e4f3
CECK <97340544+AzimAkhmedov@users.noreply.github.com>
2023-01-18 17:45:03 +0500

    feat: grades

--- server/authRouter.js
+++ server/authRouter.js
@@ -2,7 +2,7 @@ const Router = require("express");
 const router = new Router();
 const controller = require("./authController");
 const { check } = require("express-validator");
-const authMiddleware = require("./middleware/authMiddleware")
+const authMiddleware = require("./middleware/authMiddleware");
 
 router.post(
   `/registration`,
@@ -15,7 +15,7 @@ router.post(
   controller.registration
 );
 router.post(`/login`, controller.login);
-router.get(`/check`, authMiddleware,  controller.check);
+router.get(`/check`, authMiddleware, controller.check);
 router.get(`/users`, controller.getUsers);
 
 module.exports = router;
--- server/gradeController.js
+++ server/gradeController.js
@@ -1,9 +1,22 @@
-const gradingHomework = async (req, res) => {
-  const { whose, link } = req.body;
-  const isExist = await User.findOne({ whose });
+const PostHomework = require("./models/PostHomework");
+class gradeController {
+  async postGrade(req, res) {
+    const { whose, link, score } = req.body;
+    const isExist = await PostHomework.findOne({ whose });
 
-  if (!isExist) {
-    return res.json({ message: "Не можем узнать чье дз" });
+    if (!isExist) {
+      return res.json({ message: "Не можем узнать чье дз" });
+    }
+
+    const updatedHomework = new PostHomework({
+      whose,
+      link,
+      score,
+    });
+
+    await updatedHomework.replaceOne(isExist);
+    return res.json({ message: "Оценка успешно сохранена" });
   }
-  
-};
+}
+
+module.exports = new gradeController();
