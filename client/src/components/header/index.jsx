bff0edd45698b892781261baeba4adc93e42cc22
akutagawayep <dilnoza.akhmedova.icloud@gmail.com>
2023-01-21 18:30:16 +0500

    navbar update

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./header.module.scss";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const classes = [s.container];
+
+  const open = () => {
+    setIsOpen(!isOpen);
+  };
+
+  if (isOpen) {
+    classes.push(s.open);
+  }
   return (
-    // <div className={s.root}>
-    //   <div className={s.btn}>
-    //     <span></span>
-    //     <span></span>
-    //     <span></span>
-    //   </div>
-    //   <div className={s.container}>
-    //     <a href="#start">Start</a>
-    //     <a href="#about">About us</a>
-    //     <a href="#mentors">Our Mentors</a>
-    //     <a href="#students">Our Students</a>
-    //     <a href="#questions">Questions & Answers</a>
-    //   </div>
-    // </div>
-    <div className=""></div>
+    <div className={s.root}>
+      <div
+        className={s.btn}
+        onClick={() => {
+          open();
+        }}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={classes.join(" ")}>
+        <a href="#start">Start</a>
+        <a href="#about">About us</a>
+        <a href="#mentors">Our Mentors</a>
+        <a href="#students">Our Students</a>
+        <a href="#questions">Questions & Answers</a>
+      </div>
+    </div>
+    // <div className=""></div>
   );
 };
 
